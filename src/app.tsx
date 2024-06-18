09a115b6798781f7f876a92045562b8c691bbf20
奔腾 <jack435240760@gmail.com>
2024-06-18 13:03:43 +0800

    feat: add tms

--- src/app.tsx
+++ src/app.tsx
@@ -131,8 +131,8 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request: RequestConfig = {
-  // baseURL: 'https://proapi.azurewebsites.net',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://proapi.azurewebsites.net',
+  // baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
